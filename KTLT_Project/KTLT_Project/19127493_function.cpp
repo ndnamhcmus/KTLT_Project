d73c57a73a064f2e4b8061b1cf9515f7b3152cdf
NHẬT LÊ NGỌC MINH <19127493@student.hcmus.edu.vn>
2020-04-28 23:50:32 +0700

    debug t3

--- KTLT_Project/KTLT_Project/19127493_function.cpp
+++ KTLT_Project/KTLT_Project/19127493_function.cpp
@@ -5,13 +5,22 @@ void printCardsShuffling(int deck[][RANKS], const char* suits[], const char* ran
 	
 	int key = 1;
 	int time = 0;
+	bool is_found = false;
 	for (time; time < 52; time++)
 		for (int i = 0; i < SUITS; i++)
-			for (int j = 0; j < RANKS; j++)
+			for (int j = 0; j < RANKS; j++) {
 				if (deck[i][j] == key) {
 					cout << "(" << suits[i] << ", " << ranks[j] << ")" << endl;
 					key++;
+					is_found = true;
+					break;
 				}
+				if (is_found)
+				{
+					is_found = false;
+					break;
+				}
+			}
 }
 void printHand(int** hand, const char* suits[], const char* ranks[]) {
 	for (int i = 0; i < 5; i++)
